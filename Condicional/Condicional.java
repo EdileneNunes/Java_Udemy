e05b94457755ba4e82f460a971eae0138109b6a7
EdileneNunes <109703859+EdileneNunes@users.noreply.github.com>
2023-07-25 23:01:35 -0300

    Add files via upload

---
+++ Condicional/Condicional.java
@@ -0,0 +1,18 @@
+public class Condicional{
+	
+	public static void main(String args[]) {
+		
+		int idade = 21;
+		
+		boolean resutado = idade > 30;
+		
+		if(resutado) {
+			
+			System.out.println("Você é maior de idade: ");
+		}else{
+			System.out.println("Você é adulto: ");
+		}
+		
+		
+	}
+}
